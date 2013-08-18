f663f8accde50998db1d246d64f4785956ee2f1a
D <dannyqho@gmail.com>
2013-08-17 21:12:34 -0400

    added regular exp

--- lib/fuzzy_ruby.rb
+++ lib/fuzzy_ruby.rb
@@ -1,5 +1,24 @@
 class Fuzzy
-  def self.hello
-    puts "Hello World!"
+
+  def find(strings, input)
+    regexp = Regexp.new(build_regexp (input))
+    if strings =~ regexp
+      puts 'YES'
+    elsif
+      puts 'NO'
+    end
   end
+
+  private
+
+    def build_regexp(string)
+      regexp = ""
+      string.each_char do |c|
+        regexp << ".*#{c}"
+      end
+    end
+
 end
+
+f = Fuzzy.new
+f.find("test", "tes")
