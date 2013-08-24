c10811e5687641c05687347e57bef9e520156b1f
D <dannyqho@gmail.com>
2013-08-24 02:06:30 -0400

    first complete approach

--- lib/fuzzy_ruby.rb
+++ lib/fuzzy_ruby.rb
@@ -2,26 +2,42 @@ class Fuzzy
 
   def find(strings, input)
     # array to store weights of each string
-    @weights = []
+    @weights = Array.new(strings.length, -1)
 
     # array to store each string
     @strings = strings.dup
+
+    # array to return
+    ret = Array.new
 
     # build regular expression
     regexp = /#{build_regexp(input)}/
 
     # for each string, calculate weight
     @strings.each_index do |i|
-      # puts @strings[i]
       assign_weights(i, regexp)
     end
 
-    # return array of strings in decreasing weight order
+    # delete string if there is no match
+    @weights.each_index do |i|
+      if @weights[i] == -1
+        @weights.delete_at(i)
+        strings.delete_at(i)
+      end
+    end
+
+    # return array of strings with best match (least weight)
+    @weights.map.with_index.sort_by(&:first).map(&:last).each do |index|
+      ret.push strings[index]
+    end
+
+    return ret
 
   end
 
   private
 
+    # build the regular expression
     def build_regexp(string)
       regexp = ""
       string.each_char do |c|
@@ -30,11 +46,22 @@ class Fuzzy
       regexp << "(\w*)"
     end
 
+    # recursive function to assign weight to string
+    # once there is match, removes one character from the reg exp and repeat
     def assign_weights(index, regexp)
-      @weights[index] = @strings[index] =~ regexp
+      weight = @strings[index] =~ regexp
+      if weight == nil
+        return
+      end
+      @weights[index] += weight
+      if !done(index)
+        @strings[index] = @strings[index][weight+1..@strings[index].length]
+        assign_weights(index, /#{regexp.source[5..regexp.source.length]}/)
+      end
+    end
+
+    def done(index)
+      @strings[index] == nil || @weights[index] == -1 ? true : false
     end
 
 end
-
-f = Fuzzy.new
-f.find(["test", "aadsffatest"], "tes")
